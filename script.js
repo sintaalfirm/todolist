eb3d7e46c5055ae2709c871ebce0f2eaa52079fc
AdindaPutri1 <adindaputriromadhon@mail.ugm.ac.id>
2023-03-30 18:45:54 +0700

    Menambahkan script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const blink = document.getElementById('blink');
-const colors = ['red', 'blue', 'orange', 'purple'];
-let currentColorIndex = 0;
-
-setInterval(function() {
-  blink.style.color = colors[currentColorIndex];
-  currentColorIndex = (currentColorIndex + 1) % colors.length;
-}, 500);
