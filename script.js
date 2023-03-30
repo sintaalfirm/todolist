81ddd9bb68ac31ae73eebff223cf7227a5bfae7e
AdindaPutri1 <adindaputriromadhon@mail.ugm.ac.id>
2023-03-30 18:53:13 +0700

    Menambahkan script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ tombol.addEventListener('click', function() {
 
 document.body.appendChild(tombol);
 
+const blink = document.getElementById('blink');
+const colors = ['red', 'blue', 'orange', 'purple'];
+let currentColorIndex = 0;
+
+setInterval(function() {
+  blink.style.color = colors[currentColorIndex];
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
+}, 500);
