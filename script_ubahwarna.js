5cad72847cf7ecb69215383675a21cef06be7d91
AdindaPutri1 <adindaputriromadhon@mail.ugm.ac.id>
2023-03-30 18:37:48 +0700

    Menambahkan Tulisan Berubah-Ubah Warna

---
+++ script_ubahwarna.js
@@ -0,0 +1,8 @@
+const blink = document.getElementById('blink');
+const colors = ['red', 'blue', 'orange', 'purple'];
+let currentColorIndex = 0;
+
+setInterval(function() {
+  blink.style.color = colors[currentColorIndex];
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
+}, 500);
